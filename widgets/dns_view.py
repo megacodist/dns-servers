d6fa45688254073db4270f9cba7ea8e820f0a3e9
megacodist <megacodist@gmail.com>
2024-07-14 21:50:38 +0430

    Re-designing DnsView

--- widgets/dns_view.py
+++ widgets/dns_view.py
@@ -5,7 +5,9 @@
 import logging
 import tkinter as tk
 from tkinter import ttk
-from typing import Callable, Iterable, MutableSequence, TYPE_CHECKING
+from typing import Callable, Iterable, TYPE_CHECKING
+
+import tksheet
 
 from db import DnsServer
 
@@ -14,7 +16,70 @@ if TYPE_CHECKING:
     _: Callable[[str], str] = lambda a: a
 
 
-class Dnsview(tk.Frame):
+class Dnsview(tksheet.Sheet):
+    def __init__(
+            self,
+            master: tk.Misc,
+            double_clicked_cb: Callable[[], None] | None = None,
+            *,
+            name_col_width: int = 100,
+            prim_4_col_width: int = 100,
+            secon_4_col_width: int = 100,
+            prim_6_col_width: int = 100,
+            secon_6_col_width: int = 100,
+            **kwargs
+            ) -> None:
+        super().__init__(master, **kwargs)
+        self.headers([
+            _('NAME'),
+            _('PRIM_4'),
+            _('SECON_4'),
+            _('PRIM_6'),
+            _('SECON_6'),])
+        self.column_width(0, name_col_width)
+        self.column_width(1, prim_4_col_width)
+        self.column_width(2, secon_4_col_width)
+        self.column_width(3, prim_6_col_width)
+        self.column_width(4, secon_6_col_width)
+        #
+        self._cbDoubleClicked = double_clicked_cb
+        """The callback to be called if an item is double clicked."""
+        # Bindins...
+        self.enable_bindings(
+            'single_select',
+            'drag_select',
+            'select_all',
+            'column_select',
+            'row_select',
+            'ctrl_click_select',
+            'up',
+            'down',
+            'left',
+            'right',
+            'prior',
+            'next',
+            'column_width_resize',)
+    
+    def getColsWidth(self) -> tuple[int, int, int, int, int]:
+        """Returns the width of `name`, `prim_4`, and `secon_4`, 'prim_6',
+        and 'secon_6' columns width as a 5-tuple respectively.
+        """
+        return tuple([int(width) for width in self.get_column_widths()]) # type: ignore
+
+    def clear(self) -> None: # type: ignore
+        """Clears all DNS servers from the View."""
+        count = len(self.get_sheet_data())
+        for idx in reversed(range(count)):
+            self.delete_row(idx)
+    
+    def populate(self, dnses: Iterable[DnsServer]) -> None:
+        self.clear()
+        for dns in dnses:
+            self.insert_row(
+                [dns.name, dns.prim_4, dns.secon_4, dns.prim_6, dns.secon_6])
+
+
+class Dnsview__(tk.Frame):
     def __init__(
             self,
             master: tk.Misc,
